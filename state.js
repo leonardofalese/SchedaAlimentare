f1bf617f9a8e95e434218993f277a170b64bc8a2
Salvatore Leonardo Falese <160590138+leonardofalese@users.noreply.github.com>
2026-04-15 01:42:51 +0200

    Delete state.js

--- state.js
+++
@@ -1,9 +0,0 @@
-// ── STATE ─────────────────────────────────────────────────
-let state = {
-  meals: {},
-  shop: {},
-  mealData: JSON.parse(JSON.stringify(DEFAULT_MEALS)),
-  shopData: JSON.parse(JSON.stringify(DEFAULT_SHOP))
-};
-let today = new Date().getDay(); today = today===0?6:today-1;
-let currentDay = today, timerInt = null, settingsDay = 0;
